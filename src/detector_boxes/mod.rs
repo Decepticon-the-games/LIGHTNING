f13848f6a3e1a142f37d1c6c79c40aec2259ba04
Decepticon-the-games <82913158+Decepticon-the-games@users.noreply.github.com>
2021-04-20 19:44:19 -0400

    Added Sephiroth soft Up B

--- src/detector_boxes/mod.rs
+++ src/detector_boxes/mod.rs
@@ -113,6 +113,19 @@ pub fn mewtwo_up_b_air(fighter: &mut L2CFighterCommon) {
 //--------------------------------------------------------------------------------------------------------
 //Up b detector hitbox when teleported
 
+//SEPHIROTH
+#[acmd_func(
+    battle_object_category = BATTLE_OBJECT_CATEGORY_FIGHTER, 
+    battle_object_kind = FIGHTER_KIND_MEWTWO, 
+    animation = "special_hi1",
+    animcmd = "game_specialairhi")]
+pub fn seph_up_b_move(fighter: &mut L2CFighterCommon) {
+    acmd!(lua_state,{
+            
+            ATTACK(ID=1, Part=0, Bone=hash40("top"), Damage=1.0, Angle=367, KBG=100, FKB=50, BKB=0, Size=12.0, X=0.0, Y=10.0, Z=0.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=2.0, SDI=0.0, Clang_Rebound=ATTACK_SETOFF_KIND_OFF, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_S, SFXType=COLLISION_SOUND_ATTR_NONE, Type=ATTACK_REGION_NONE)
+       
+        });
+}
 
 
 pub fn install() {
@@ -120,7 +133,8 @@ pub fn install() {
         falco_side_b_air,
         falco_side_b,
         mewtwo_up_b,
-        mewtwo_up_b_air
+        mewtwo_up_b_air,
+        seph_up_b_move
         
     );
 }
--- src/lightning_edge/mod.rs
+++ src/lightning_edge/mod.rs
@@ -16,15 +16,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
         let lua_state = fighter.lua_state_agent; 
         
         
-        if fighter_kind == *FIGHTER_KIND_EDGE {
-            //if MotionModule::motion_kind(module_accessor) == smash::hash40("special_hi1") 
-            //{
-            //    acmd!(lua_state,{
-                    
-            //        ATTACK(ID=1, Part=0, Bone=hash40("top"), Damage=1.0, Angle=367, KBG=100, FKB=50, BKB=0, Size=12.0, X=0.0, Y=10.0, Z=0.0, X2=LUA_VOID, Y2=LUA_VOID, Z2=LUA_VOID, Hitlag=2.0, SDI=0.0, Clang_Rebound=ATTACK_SETOFF_KIND_OFF, FacingRestrict=ATTACK_LR_CHECK_POS, SetWeight=false, ShieldDamage=0, Trip=0.0, Rehit=0, Reflectable=false, Absorbable=false, Flinchless=false, DisableHitlag=false, Direct_Hitbox=true, Ground_or_Air=COLLISION_SITUATION_MASK_GA, Hitbits=COLLISION_CATEGORY_MASK_ALL, CollisionPart=COLLISION_PART_MASK_ALL, FriendlyFire=false, Effect=hash40("collision_attr_normal"), SFXLevel=ATTACK_SOUND_LEVEL_S, SFXType=COLLISION_SOUND_ATTR_NONE, Type=ATTACK_REGION_NONE)
-                    
-            //  });
-            //}
+        
             if MotionModule::motion_kind(module_accessor) == smash::hash40("special_hi1_end") && MotionModule::frame(module_accessor) >1.0 {
                 CancelModule::enable_cancel(module_accessor);
             }

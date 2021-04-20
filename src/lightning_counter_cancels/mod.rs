6c972cf63ba5ab1a84d9c7ff06d548aff531063c
Decepticon-the-games <82913158+Decepticon-the-games@users.noreply.github.com>
2021-04-20 19:36:05 -0400

    All counters can cancel upon being hit

--- src/lightning_counter_cancels/mod.rs
+++ src/lightning_counter_cancels/mod.rs
@@ -13,7 +13,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
         let situation_kind = smash::app::lua_bind::StatusModule::situation_kind(module_accessor);
         let cat1 = ControlModule::get_command_flag_cat(module_accessor, 0);
         
-        
+        //MARTH, LUCINA, ROY, CHROM, SEPHIROTH, CORRIN
         if (fighter_kind == *FIGHTER_KIND_MARTH 
         || fighter_kind == *FIGHTER_KIND_LUCINA
         || fighter_kind == *FIGHTER_KIND_ROY
@@ -31,6 +31,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
                 }
             }   
         }
+        //SHULK
         if fighter_kind == *FIGHTER_KIND_SHULK {
             if (MotionModule::motion_kind(module_accessor) == smash::hash40("special_lw_hit") || MotionModule::motion_kind(module_accessor) == smash::hash40("special_air_lw_hit")) {
                 if (cat1 & (*FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_LW) != 0) {
@@ -40,6 +41,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
                 }
             }
         }
+        //KROOL
         if fighter_kind == *FIGHTER_KIND_KROOL {
             if (MotionModule::motion_kind(module_accessor) == smash::hash40("special_lw_hit") || MotionModule::motion_kind(module_accessor) == smash::hash40("special_air_lw_hit")) {
                 if MotionModule::frame(module_accessor) >2.0 {
@@ -49,7 +51,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
                 }
             }   
         }
-
+        //LUCARIO
         if fighter_kind == *FIGHTER_KIND_LUCARIO {
             if MotionModule::motion_kind(module_accessor) == smash::hash40("special_lw_split") {
                 if MotionModule::frame(module_accessor) >1.0 {
@@ -59,7 +61,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
                 }
             }   
         }
-
+        //GRENINJA
         if fighter_kind == *FIGHTER_KIND_GEKKOUGA {
             if (MotionModule::motion_kind(module_accessor) == smash::hash40("special_lw_attack") || MotionModule::motion_kind(module_accessor) == smash::hash40("special_air_lw_attack")) {
                 if MotionModule::frame(module_accessor) >1.0 {
@@ -69,7 +71,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
                 }
             }   
         }
-
+        //JOKER
         if fighter_kind == *FIGHTER_KIND_JACK {
             if (MotionModule::motion_kind(module_accessor) == smash::hash40("special_lw_counter") || MotionModule::motion_kind(module_accessor) == smash::hash40("special_air_lw_counter")) {
                 if MotionModule::frame(module_accessor) >1.0 {
@@ -79,7 +81,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
                 }
             }   
         }
-
+        //MIISWORDFIGHTER
         if fighter_kind == *FIGHTER_KIND_MIISWORDSMAN {
             if (MotionModule::motion_kind(module_accessor) == smash::hash40("special_lw1_hit") || MotionModule::motion_kind(module_accessor) == smash::hash40("special_air_lw1_hit")) {
                 if MotionModule::frame(module_accessor) >1.0 {
@@ -89,7 +91,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
                 }
             }   
         }
-
+        //PALUTENA
         if fighter_kind == *FIGHTER_KIND_PALUTENA {
             if (MotionModule::motion_kind(module_accessor) == smash::hash40("special_lw_attack") || MotionModule::motion_kind(module_accessor) == smash::hash40("special_air_lw_attack")) {
                 if MotionModule::frame(module_accessor) >1.0 {
@@ -99,7 +101,7 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
                 }
             }   
         }
-        
+        //PEACH, DAISY
         if (fighter_kind == *FIGHTER_KIND_PEACH
         || fighter_kind == *FIGHTER_KIND_DAISY) {
             if (MotionModule::motion_kind(module_accessor) == smash::hash40("special_n_hit") || MotionModule::motion_kind(module_accessor) == smash::hash40("special_air_n_hit")) {

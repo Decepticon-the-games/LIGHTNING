fbfaaa5b421452e7625eebea2b34f759d772945b
Decepticon-the-games <82913158+Decepticon-the-games@users.noreply.github.com>
2021-04-23 11:06:53 -0400

    Free Up Specials (like sonic)

---
+++ src/lightning_upbtransitions/mod.rs
@@ -0,0 +1,39 @@
+use smash::app::lua_bind::StatusModule::*;
+use smash::app::lua_bind::*;
+use smash::lua2cpp::{L2CFighterCommon, L2CFighterBase};
+use smash::lib::lua_const::*;
+use acmd;
+
+static mut UP_SPECIAL : [bool; 8] = [false; 8];
+static mut ENTRY_ID : usize = 0;
+pub fn set_bool(fighter: &mut L2CFighterCommon) {
+    unsafe {
+        let lua_state = fighter.lua_state_agent;
+        let module_accessor = smash::app::sv_system::battle_object_module_accessor(lua_state);
+        ENTRY_ID = WorkModule::get_int(module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
+        if StatusModule::status_kind(module_accessor) == *FIGHTER_STATUS_KIND_SPECIAL_HI {
+            UP_SPECIAL[ENTRY_ID] = true;
+        }
+    }
+}
+
+#[skyline::hook(replace=smash::app::lua_bind::WorkModule::is_enable_transition_term)]
+pub unsafe fn is_enable_transition_term_replace(module_accessor: &mut BattleObjectModuleAccessor, term: i32) -> bool {
+    let ret = original!()(module_accessor,term);
+    ENTRY_ID = WorkModule::get_int(module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
+    if UP_SPECIAL[entry_id(module_accessor)] {
+         if term == *FIGHTER_STATUS_TRANSITION_TERM_ID_CONT_SPECIAL_HI {
+             return false;
+         }
+         else {
+             return ret;
+         }
+     }
+     else {
+         return ret;
+     }
+}
+pub fn install() {
+    acmd::add_custom_hooks!(set_bool);
+    skyline::install_hook!(is_enable_transition_term_replace);
+}

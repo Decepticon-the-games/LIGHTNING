8ecdb122d67ae362e845229a2f9623b7b8e20958
Decepticon-the-games <82913158+Decepticon-the-games@users.noreply.github.com>
2021-04-23 11:10:26 -0400

    Seph Blade Dash Cancel

--- src/lightning_edge/mod.rs
+++ src/lightning_edge/mod.rs
@@ -17,21 +17,21 @@ pub fn once_per_fighter_frame(fighter : &mut L2CFighterCommon) {
         
         
         
-            if MotionModule::motion_kind(module_accessor) == smash::hash40("special_hi1_end") && MotionModule::frame(module_accessor) >1.0 {
+        if MotionModule::motion_kind(module_accessor) == smash::hash40("special_hi1_end") && MotionModule::frame(module_accessor) >1.0 {
+            CancelModule::enable_cancel(module_accessor);
+        }
+            
+            
+    
+        else if ! (status_kind == *FIGHTER_STATUS_KIND_SPECIAL_HI)
+        && ! (status_kind == *FIGHTER_STATUS_KIND_CATCH_ATTACK)
+        && ! (status_kind == *FIGHTER_STATUS_KIND_ATTACK)
+        && ! (status_kind == *FIGHTER_STATUS_KIND_ATTACK_100) {
+            if AttackModule:: is_attack_occur(module_accessor) {
                 CancelModule::enable_cancel(module_accessor);
             }
-                
-                
-        
-            else if ! (status_kind == *FIGHTER_STATUS_KIND_SPECIAL_HI)
-            && ! (status_kind == *FIGHTER_STATUS_KIND_CATCH_ATTACK)
-            && ! (status_kind == *FIGHTER_STATUS_KIND_ATTACK)
-            && ! (status_kind == *FIGHTER_STATUS_KIND_ATTACK_100) {
-                if AttackModule:: is_attack_occur(module_accessor) {
-                    CancelModule::enable_cancel(module_accessor);
-                }
-            }
         }
+        
     }                                      
 }
 

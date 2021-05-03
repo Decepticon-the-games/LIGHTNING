ebd635ca3c567ad57feca7ff807cf4def6a05aa2
Decepticon-the-games <82913158+Decepticon-the-games@users.noreply.github.com>
2021-05-03 08:18:11 -0400

    fixed up b resets... again

--- src/lightning_01_upbtransitions/mod.rs
+++ src/lightning_01_upbtransitions/mod.rs
@@ -166,7 +166,8 @@ pub fn set_bool(fighter: &mut L2CFighterCommon) {
         || status_kind == *FIGHTER_JACK_STATUS_KIND_SPECIAL_HI_PULL
         || status_kind == *FIGHTER_JACK_STATUS_KIND_SPECIAL_HI_THROW
         || status_kind == *FIGHTER_JACK_STATUS_KIND_SPECIAL_HI_PICKUP
-        || 
+        || StopModule::is_damage(module_accessor)
+        || StopModule::is_stop(module_accessor)
         || status_kind == *FIGHTER_STATUS_KIND_THROWN
         || status_kind == *FIGHTER_STATUS_KIND_REBIRTH
         || status_kind == *FIGHTER_STATUS_KIND_CLUNG_DIDDY

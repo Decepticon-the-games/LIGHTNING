81c3c41711aec9f6f4c2f93cdf78d2111a6497b6
WuBoy <alexwuief@gmail.com>
2021-07-23 19:14:48 -0400

    future-proofed notify_log_event_collision_hit

--- src/hooks.rs
+++ src/hooks.rs
@@ -3,8 +3,28 @@ use smash::lib::lua_const::*;
 use smash::app::lua_bind::*;
 use smash::app::FighterManager;
 use smash::app::*;
+use skyline::hooks::{getRegionAddress, Region};
 use crate::lightning_01_ultrainstinct::SECRET_SENSATION;
 use crate::lightning_01_upbtransitions::UP_SPECIAL;
+
+static NOTIFY_LOG_EVENT_COLLISION_HIT_SEARCH_CODE: &[u8] = &[
+    0xff, 0x03, 0x03, 0xd1,
+    0xe8, 0x2b, 0x00, 0xfd,
+    0xfc, 0x6f, 0x06, 0xa9,
+    0xfa, 0x67, 0x07, 0xa9,
+    0xf8, 0x5f, 0x08, 0xa9,
+    0xf6, 0x57, 0x09, 0xa9,
+    0xf4, 0x4f, 0x0a, 0xa9,
+    0xfd, 0x7b, 0x0b, 0xa9,
+    0xfd, 0xc3, 0x02, 0x91,
+    0xfb, 0x03, 0x00, 0xaa
+];
+
+macro_rules! c_str {
+    ($l:tt) => {
+        [$l.as_bytes(), "\u{0}".as_bytes()].concat().as_ptr();
+    };
+}
 
 #[skyline::hook(replace = smash::app::lua_bind::WorkModule::is_enable_transition_term )]
 pub unsafe fn is_enable_transition_term_replace(module_accessor: &mut BattleObjectModuleAccessor, term: i32) -> bool {
@@ -29,5 +49,13 @@ pub unsafe fn is_enable_transition_term_replace(module_accessor: &mut BattleObje
 
 }
 pub fn install() {
+    unsafe{
+        let text_ptr = getRegionAddress(Region::Text) as *const u8;
+        let text_size = (getRegionAddress(Region::Rodata) as usize) - (text_ptr as usize);
+        let text = std::slice::from_raw_parts(text_ptr, text_size);
+        if let Some(offset) = find_subsequence(text, NOTIFY_LOG_EVENT_COLLISION_HIT_SEARCH_CODE) {
+            NOTIFY_LOG_EVENT_COLLISION_HIT_OFFSET = offset;
+        }
+    }
     skyline::install_hook!(is_enable_transition_term_replace);
 }

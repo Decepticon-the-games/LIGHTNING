135a934b282c06257517424448e89cdd57d1dc08
Decepticon-the-games <82913158+Decepticon-the-games@users.noreply.github.com>
2021-12-15 16:10:34 -0500

    fixed miis

--- src/lightning_mii/mod.rs
+++ src/lightning_mii/mod.rs
@@ -12,13 +12,13 @@ fn once_per_fighter_frame_1(fighter : &mut L2CFighterCommon) {
         let status_kind = smash::app::lua_bind::StatusModule::status_kind(module_accessor);
         let situation_kind = smash::app::lua_bind::StatusModule::situation_kind(module_accessor);
         let cat1 = ControlModule::get_command_flag_cat(module_accessor, 0);
+        let cat2 = ControlModule::get_command_flag_cat(module_accessor, 1);
         let jump_guard_dash_upspecial_pressed = (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP) != 0 || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_WALK) != 0 || (situation_kind == *SITUATION_KIND_GROUND && (cat2 & *FIGHTER_PAD_CMD_CAT2_FLAG_COMMON_GUARD) != 0) || (situation_kind == *SITUATION_KIND_AIR && (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_AIR_ESCAPE) != 0) || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_DASH) != 0 || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_TURN_DASH) != 0 || (situation_kind == *SITUATION_KIND_AIR && (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI) != 0);
-    let frame = MotionModule::frame(module_accessor);
+        let frame = MotionModule::frame(module_accessor);
         
         //FIXES
         //-------------------------------------------------------------------------------
-        if (status_kind == *FIGHTER_MIIFIGHTER_STATUS_KIND_SPECIAL_N2_ATTACK) && frame >= 17.0 
-        || (status_kind == *FIGHTER_MIIFIGHTER_STATUS_KIND_SPECIAL_S1_END) && frame >= 25.0
+        if (status_kind == *FIGHTER_MIIFIGHTER_STATUS_KIND_SPECIAL_S1_END) && frame >= 25.0
         {
             if AttackModule:: is_attack_occur(fighter.module_accessor) && ! SlowModule::is_slow(module_accessor){
                 if  jump_guard_dash_upspecial_pressed {
@@ -28,7 +28,7 @@ fn once_per_fighter_frame_1(fighter : &mut L2CFighterCommon) {
         }
 
         else if (status_kind == *FIGHTER_STATUS_KIND_CATCH_ATTACK)
-        && ! (status_kind == *FIGHTER_MIIFIGHTER_STATUS_KIND_SPECIAL_N2_ATTACK)
+        && ! (status_kind == *FIGHTER_MIIFIGHTER_STATUS_KIND_SPECIAL_N2_HIT)
         && ! (status_kind == *FIGHTER_MIIFIGHTER_STATUS_KIND_SPECIAL_S1_END)
         && ! (status_kind == *FIGHTER_STATUS_KIND_ATTACK)
         && ! (status_kind == *FIGHTER_STATUS_KIND_ATTACK_100)
@@ -47,8 +47,9 @@ fn once_per_fighter_frame_2(fighter : &mut L2CFighterCommon) {
         let status_kind = smash::app::lua_bind::StatusModule::status_kind(module_accessor);
         let situation_kind = smash::app::lua_bind::StatusModule::situation_kind(module_accessor);
         let cat1 = ControlModule::get_command_flag_cat(module_accessor, 0);
+        let cat2 = ControlModule::get_command_flag_cat(module_accessor, 1);
         let jump_guard_dash_upspecial_pressed = (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP) != 0 || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_WALK) != 0 || (situation_kind == *SITUATION_KIND_GROUND && (cat2 & *FIGHTER_PAD_CMD_CAT2_FLAG_COMMON_GUARD) != 0) || (situation_kind == *SITUATION_KIND_AIR && (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_AIR_ESCAPE) != 0) || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_DASH) != 0 || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_TURN_DASH) != 0 || (situation_kind == *SITUATION_KIND_AIR && (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI) != 0);
-    let frame = MotionModule::frame(module_accessor);
+        let frame = MotionModule::frame(module_accessor);
         
         //FIXES
         //-------------------------------------------------------------------------------
@@ -71,13 +72,14 @@ fn once_per_fighter_frame_3(fighter : &mut L2CFighterCommon) {
         let status_kind = smash::app::lua_bind::StatusModule::status_kind(module_accessor);
         let situation_kind = smash::app::lua_bind::StatusModule::situation_kind(module_accessor);
         let cat1 = ControlModule::get_command_flag_cat(module_accessor, 0);
+        let cat2 = ControlModule::get_command_flag_cat(module_accessor, 1);
         let jump_guard_dash_upspecial_pressed = (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP) != 0 || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_WALK) != 0 || (situation_kind == *SITUATION_KIND_GROUND && (cat2 & *FIGHTER_PAD_CMD_CAT2_FLAG_COMMON_GUARD) != 0) || (situation_kind == *SITUATION_KIND_AIR && (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_AIR_ESCAPE) != 0) || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_DASH) != 0 || (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_TURN_DASH) != 0 || (situation_kind == *SITUATION_KIND_AIR && (cat1 & *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI) != 0);
-    let frame = MotionModule::frame(module_accessor);
+        let frame = MotionModule::frame(module_accessor);
         
         //FIXES
         //-------------------------------------------------------------------------------
-        if (status_kind == *FIGHTER_MIISWORDSMAN_STATUS_KIND_SPECIAL_N3) && frame >32.0  
-        || (status_kind == *FIGHTER_MIISWORDSMAN_STATUS_KIND_SPECIAL_N3_MAX) && frame >32.0  
+        if (status_kind == *FIGHTER_MIISWORDSMAN_STATUS_KIND_SPECIAL_N3_END) && frame >32.0  
+        || (status_kind == *FIGHTER_MIISWORDSMAN_STATUS_KIND_SPECIAL_N3_END_MAX) && frame >32.0  
         {
             if AttackModule:: is_attack_occur(fighter.module_accessor) && ! SlowModule::is_slow(module_accessor){
                 if  jump_guard_dash_upspecial_pressed {
@@ -86,7 +88,8 @@ fn once_per_fighter_frame_3(fighter : &mut L2CFighterCommon) {
             }
         }
         else if (status_kind == *FIGHTER_STATUS_KIND_CATCH_ATTACK)
-        && ! (status_kind == *FIGHTER_MIISWORDSMAN_STATUS_KIND_SPECIAL_N3)
+        && ! (status_kind == *FIGHTER_MIISWORDSMAN_STATUS_KIND_SPECIAL_N3_END)
+        && ! (status_kind == *FIGHTER_MIISWORDSMAN_STATUS_KIND_SPECIAL_N3_END_MAX)
         && ! (status_kind == *FIGHTER_STATUS_KIND_ATTACK)
         && ! (status_kind == *FIGHTER_STATUS_KIND_ATTACK_100)
         && ! (status_kind == *FIGHTER_STATUS_KIND_THROW) {
@@ -98,5 +101,5 @@ fn once_per_fighter_frame_3(fighter : &mut L2CFighterCommon) {
 }
 
 pub fn install() {
-    smashline::install_agent_frames!(once_per_fighter_frame);
+    smashline::install_agent_frames!(once_per_fighter_frame_1,once_per_fighter_frame_2,once_per_fighter_frame_3);
 }

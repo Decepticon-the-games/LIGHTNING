7d2ff96fca1f9e564a5c41bc620c99507e0586c3
Decepticon-the-games <dawsonanderson51@gmail.com>
2022-12-31 20:10:33 -0500

    fighter template fix

--- src/fighters/fighter template/opff.rs
+++ src/fighters/fighter template/opff.rs
@@ -12,14 +12,9 @@ use {
 use crate::fighters::common::mechanics::attack_cancels::ENABLE_ATTACK_CANCEL;
 
 
-
-
-
-
 #[fighter_frame( agent = FIGHTER_KIND_CODENAMEHERE )]
 
-//Enable cancel subtitle here.
-    pub fn enable_cancel(fighter : &mut L2CFighterCommon) {
+    pub fn codenamehere_opff(fighter : &mut L2CFighterCommon) {
         unsafe {
             let entry_id = WorkModule::get_int(fighter.module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
             let module_accessor = smash::app::sv_system::battle_object_module_accessor(fighter.lua_state_agent);
@@ -29,8 +24,10 @@ use crate::fighters::common::mechanics::attack_cancels::ENABLE_ATTACK_CANCEL;
             ////let situation_kind = smash::app::lua_bind::StatusModule::situation_kind(module_accessor);
             //let cat1 = ControlModule::get_command_flag_cat(module_accessor, 0);
             //let cat2 = ControlModule::get_command_flag_cat(module_accessor, 1);
-            
-            else if for every new move.  
+
+//Enable cancel   
+
+            //else if for every new move.  
             if move_here {
                 
                 if frame >= 0.0 {
@@ -43,17 +40,14 @@ use crate::fighters::common::mechanics::attack_cancels::ENABLE_ATTACK_CANCEL;
             else {//This stays at the bottom
                 ENABLE_ATTACK_CANCEL[entry_id] = true;
             }
-        }
-    }
 
-//New fn for any other code, if not applicable just delete the lines
-    pub fn subtitlehere(fighter : &mut L2CFighterCommon) {
-        unsafe {
+
+//New subtititle for any other code, if not applicable just delete the lines
 
         }
     }
 
 pub fn install() {
-    smashline::install_agent_frames!(enable_cancel, subtitlehere);
+    smashline::install_agent_frames!(codenamehere_opff);
 
 }
